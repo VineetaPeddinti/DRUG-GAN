d11603b823005dffb78c6fac7bc896f481149d4a
Vineeta Peddinti <vineetapeddinti@gmail.com>
2020-04-27 18:41:09 -0400

    Revising the model

--- GANModel.py
+++ GANModel.py
@@ -1,16 +1,13 @@
-from tensorflow.keras.layers import Input, Dense,LSTM, Reshape, Flatten, Dropout,Bidirectional
-from tensorflow.keras.layers import BatchNormalization, Activation
-from tensorflow.keras.layers import LeakyReLU
-from tensorflow.keras.models import Sequential, Model
+from keras.layers import Input, Dense,LSTM, Reshape, Flatten, Dropout,CuDNNLSTM,Bidirectional
+from keras.layers import BatchNormalization, Activation
+from keras.layers.advanced_activations import LeakyReLU
+from keras.models import Sequential, Model
 from numpy import savetxt
 from numpy import loadtxt
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from tensorflow.keras.optimizers import Adam
 from tqdm import tqdm
 import numpy as np
-from time import time
-import datetime
-from tensorflow.keras.callbacks import TensorBoard
 
 class GANModel():
     def __init__(self,max_len,table_len,int_to_smi):
@@ -23,7 +20,7 @@ class GANModel():
         self.table_len = table_len
         self.latent_dim = 100
         self.int_to_smi = int_to_smi
-        optimizer = Adam(0.00009, 0.5)
+        optimizer = Adam(0.0002, 0.5)
 
 
         self.discriminator = self.discriminator()
@@ -41,14 +38,6 @@ class GANModel():
         validity = self.discriminator(img) #Validate images as real/ fake
         self.gan = Model(z, validity)
         self.gan.compile(loss='binary_crossentropy', optimizer=optimizer)
-        self.tensorboard = TensorBoard(
-            log_dir="\\content\\drive\\My Drive\\DRUG-GAN\\logs\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"),
-            histogram_freq=0,
-            batch_size=256,
-            write_graph=True,
-            write_grads=True
-            )
-        self.tensorboard.set_model(self.discriminator)
 
     def generator(self):
         model = Sequential()
@@ -87,23 +76,16 @@ class GANModel():
             real_data = X_train[idx]
             d_loss_real = self.discriminator.train_on_batch(real_data, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_data, fake)
-            self.tensorboard.on_epoch_end(epoch, self.named_logs(self.discriminator, d_loss_fake))
             g_loss = self.gan.train_on_batch(noise, valid)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
             print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
-        self.tensorboard.on_train_end(None)
         print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
         self.discriminator.trainable = False
-        self.gan.save("/content/drive/My Drive/DRUG-GAN/gan.h5")
+        self.gan.save("gan.h5")
         self.discriminator.trainable = True
-        self.generator.save("/content/drive/My Drive/DRUG-GAN/generator.h5")
-        self.discriminator.save("/content/drive/My Drive/DRUG-GAN/discriminator.h5")
-    def named_logs(self,model, logs):
-        result = {}
-        for l in zip(model.metrics_names, logs):
-            result[l[0]] = l[1]
-        return result
+        self.generator.save("generator.h5")
+        self.discriminator.save("discriminator.h5")
     def sample_images(self):
         r, c = 10, 10
         noise = np.random.normal(0, 1, (r * c, self.latent_dim))
@@ -111,7 +93,7 @@ class GANModel():
         print(type(gen_imgs))
         print(gen_imgs.shape)
         print(gen_imgs)
-        f = open("/content/drive/My Drive/DRUG-GAN/pred_smiles.txt","w")
+        f = open("pred_smiles.txt","w")
         for i in gen_imgs:
             pred_smiles = [((self.table_len/2) * x) + (self.table_len/2) for x in i]
             pred_smiles = [self.int_to_smi[int(x)] for x in pred_smiles]
@@ -122,9 +104,9 @@ class GANModel():
         f.close()
         print(pred_smiles)
     def load_models(self):
-        discriminator = load_model('/content/drive/My Drive/DRUG-GAN/discriminator.h5')
-        generator = load_model('/content/drive/My Drive/DRUG-GAN/generator.h5')
-        gan = load_model('/content/drive/My Drive/DRUG-GAN/gan.h5')
+        discriminator = load_model('discriminator.h5')
+        generator = load_model('generator.h5')
+        gan = load_model('gan.h5')
         gan.summary()
         discriminator.summary()
         generator.summary()
